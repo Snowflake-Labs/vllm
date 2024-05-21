cb346b9ca8adbbf6a29b22b7d81b6cfaa344fa06
sfc-gh-hazhang <hao.zhang@snowflake.com>
2024-05-21 22:41:43 +0000

    add new benchmarking scripts

---
+++ benchmarks/start_benchmark_server.sh
@@ -0,0 +1,8 @@
+python -m vllm.entrypoints.api_server \
+	--model /data-fast/yak2b-instruct \
+       	--swap-space 16 \
+	--disable-log-requests \
+	--tensor-parallel-size 8 \
+	--pipeline-parallel-size 2 \
+        --enforce-eager \
+	--quantization deepspeedfp
---
+++ benchmarks/test_pp.sh
@@ -0,0 +1,6 @@
+python benchmark_online.py \
+	--prompt_length 2048 \
+	--max_new_tokens 256 \
+	-qps 1.0 \
+	--framework vllm \
+	--model /data-fast/yak-700m 
