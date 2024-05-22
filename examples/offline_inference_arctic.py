001c00b31839c1fbbfb8d38b984661d232092f81
Hojae Han <hojae.han@snowflake.com>
2024-05-22 11:58:55 -0700

    yak2c vllm hello world

--- examples/offline_inference_arctic.py
+++ examples/offline_inference_arctic.py
@@ -11,10 +11,11 @@ prompts = [
 sampling_params = SamplingParams(temperature=0.8, top_p=0.95)
 
 # Create an LLM.
-arctic_model_path = "/checkpoint/arctic"
+arctic_model_path = "/notebooks/yak2c"
 llm = LLM(model=arctic_model_path,
           quantization="deepspeedfp",
-          tensor_parallel_size=8)
+          trust_remote_code=True,
+          tensor_parallel_size=2)
 # Generate texts from the prompts. The output is a list of RequestOutput objects
 # that contain the prompt, generated text, and other information.
 
